58b1392f914f054042778e3e73fd3d1f9652e73f
minhtri5302 <minhtrithcslqd1@gmail.com>
2021-05-02 12:33:32 +0700

    Start

---
+++ main.cpp
@@ -0,0 +1,23 @@
+//Trương Minh Trí
+#include <bits/stdc++.h>
+#define FOR(i, x, y) for(int i = x; i <= y; ++i)
+#define FORN(i, x, y) for(int i = x; i >= y; --i)
+#define Task "LT"
+#define maxn
+#define maxm
+#define pii pair <int, int>
+#define pll pair <long long, long long>
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0);cout.tie(0);
+    //freopen(Task".inp", "r", stdin);
+    //freopen(Task".out", "w", stdout);
+
+
+}
